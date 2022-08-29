fdb522252b0980ebfc18d79024689940baeb6100
mostafamt <mostafamt60@yahoo.com>
2022-08-29 13:52:01 +0200

    edit server port

--- server.js
+++ server.js
@@ -30,6 +30,6 @@ const server = http.createServer((req, res) => {
   }
 });
 
-const PORT = 5000;
+const PORT = 5000 || process.env.PORT;
 
 server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
